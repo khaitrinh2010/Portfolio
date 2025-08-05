790f0bf95cf05d328d2d093b859301437d70e540
khai <trinhlamkhai@gmail.com>
2025-08-05 20:04:47 +0700

    Finalize

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -12,7 +12,7 @@ const navItems = [
 
 export const NavBar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
-    const [isMenuOpen, setIsMenuOpen] = useState(true);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
